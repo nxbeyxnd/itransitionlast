81397aec7a5c26362d6646d8218a9a1c1cebc219
nxbeyxnd <unblocksteam@gmail.com>
2021-10-13 00:12:06 +0300

    added api/v1/task/add annotastion @ResponseStatus

--- src/main/resources/settings.js
+++
@@ -1,8 +0,0 @@
-S3handler = new S3handler(System.getenv("googleClientID"))
-
-const aws = require('aws-sdk');
-
-let s3 = new aws.S3({
-    accessKeyId: process.env.googleClientId,
-    secretAccessKey: process.env.googleClientSecret
-});
